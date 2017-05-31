f626a0a9ff82e27eeee2feb73f3d64729d6137f3
Jordan Winkelman <jordanwink201@gmail.com>
2017-05-31 15:53:03 -0700

    initial commit

---
+++ app/cache/prod/appParameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'localhost',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => 'root',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'A8LuJJK9fymybzRmCFgp4NI1j1hhRCYcWfMBt9V8ywAFEedjaDFG7aDj',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2017-06-01',
+    'locale' => 'en-US',
+    'cookie_key' => 'fpzijKVfpiUUeCHyyCEkqrAqLWwRUnDuaeznnQPYoEg9nn3wbv8zAP0G',
+    'cookie_iv' => 'mfy6BCXq',
+    'new_cookie_key' => 'def0000010912e8823529c73944ca19f53596401a658188e723ab597044d22e9a7ce5e202ed4848c4d8e83807bb8c147e75a647677980a1d38cc9e7d7065e3dcfcf042f0',
+  ),
+);
